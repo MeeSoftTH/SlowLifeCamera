fb8c3c5af5d85e688c0e64cbb69cd46894f1edd8
Pawarit Bunrith <pawarit.b@Pawarit-Bunriths-MacBook-Pro.local>
2015-07-03 11:55:11 +0700

    Group type class

---
+++ SlowLifeCamera/SaveState/save.swift
@@ -0,0 +1,13 @@
+//
+//  save.swift
+//  SlowLifeCamera
+//
+//  Created by Pawarit_Bunrith on 7/3/2558 BE.
+//  Copyright (c) 2558 Pawarit_Bunrith. All rights reserved.
+//
+
+import UIKit
+
+class save: NSObject {
+   
+}
